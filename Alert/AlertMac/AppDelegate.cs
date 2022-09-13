03e96da104aa96aefacefe8572c29ecf9b3dd3b7
김태오 <38588097+Sunday5214@users.noreply.github.com>
2022-09-13 22:49:55 +0900

    add. project 추가

---
+++ Alert/AlertMac/AppDelegate.cs
@@ -0,0 +1,50 @@
+﻿using Alert;
+using AppKit;
+using Foundation;
+using SwiftToSharpBinding;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace AlertMac
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : FormsApplicationDelegate
+    { 
+        NSWindow window;
+        private AlertTesterProxy _proxy;
+
+        public AppDelegate ()
+		{
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "Xamarin.Forms on Mac!"; // choose your own Title here
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow
+        {
+            get { return window; }
+        }
+
+        public override void DidFinishLaunching (NSNotification notification)
+		{
+            // Insert code here to initialize your application
+            Forms.Init();
+            LoadApplication(new App());
+
+            _proxy = new AlertTesterProxy();
+            var result = _proxy.ErrorReadingResultsWithQuestion("qwe", "qwe123");
+          //  ErrorReadingResult
+
+            base.DidFinishLaunching(notification);
+        }
+
+		public override void WillTerminate (NSNotification notification)
+		{
+			// Insert code here to tear down your application
+		}
+	}
+}
+
---
+++ Alert/AlertTestProxyBinding/ApiDefinition.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+
+namespace SwiftToSharpBinding
+{
+    // @interface AlertTesterProxy : NSObject
+    [BaseType(typeof(NSObject))]
+    interface AlertTesterProxy
+    {
+        // -(BOOL)errorReadingResultsWithQuestion:(NSString * _Nonnull)question text:(NSString * _Nonnull)text __attribute__((warn_unused_result("")));
+        [Export("errorReadingResultsWithQuestion:text:")]
+        bool ErrorReadingResultsWithQuestion(string question, string text);
+    }
+}
+
